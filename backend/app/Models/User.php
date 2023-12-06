8ee0762f45d6c3c94ee16a3502fae949c52e4a3a
fadl hasn <fadl@fadl>
2023-12-06 22:33:04 +0300

    auth register and login api by passport

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,11 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
+use Laravel\passport\HasApiTokens;
+require_once __DIR__.'/../../vendor/laravel/passport/src/HasApiTokens.php';
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens,HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function role(){
+        $this->belongsTo(Role::class);
+    }
 }
+
+
+//

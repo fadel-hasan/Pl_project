fa6e330d91872b476da520211be7ece5cf1e1e38
Rufida123 <rufidazaza@gmail.com>
2023-12-23 10:29:47 +0300

    added validation to the login

--- frontend/app/lib/pages/login_page.dart
+++ frontend/app/lib/pages/login_page.dart
@@ -7,106 +7,109 @@ class LoginPage extends StatelessWidget {
 
   final TextEditingController? _emailController = TextEditingController();
   final TextEditingController? _passwordController = TextEditingController();
-
+  GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 227, 247, 247),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          children: [
-            Spacer(
-              flex: 2,
-            ),
-            CircleAvatar(
-              radius: 122,
-              backgroundColor: Colors.black,
-              child: CircleAvatar(
-                radius: 120,
-                backgroundImage:
-                    AssetImage('assets/photo_2023-12-04_12-24-38.jpg'),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Spacer(
+                flex: 2,
               ),
-            ),
-            Text(
-              'Pharmacy App',
-              style: TextStyle(
-                fontSize: 32,
-                color: Color(0xff17A4A1),
-                fontFamily: 'Pacifico',
+              CircleAvatar(
+                radius: 122,
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                  radius: 120,
+                  backgroundImage:
+                      AssetImage('assets/photo_2023-12-04_12-24-38.jpg'),
+                ),
               ),
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            Row(
-              children: [
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Color(0xff17A4A1),
-                  ),
+              Text(
+                'Pharmacy App',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Color(0xff17A4A1),
+                  fontFamily: 'Pacifico',
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CustomTextField(
-              hintText: 'Email',
-              controller: _emailController,
-              obscureText: false,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Password',
-              controller: _passwordController,
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, 'Medicine');
-                },
-                child: CustomButton(
-                  text: 'LOGIN',
-                  width: double.infinity,
-                  height: 60,
-                )),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'dont\'t have an account?',
-                  style: TextStyle(
-                    color: Color(0xff17A4A1),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, 'RegisterPage');
-                  },
-                  child: Text(
-                    ' Register',
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'LOGIN',
                     style: TextStyle(
-                      color: Colors.black,
+                      fontSize: 24,
+                      color: Color(0xff17A4A1),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Spacer(
-              flex: 3,
-            ),
-          ],
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                hintText: 'Email',
+                controller: _emailController,
+                obscureText: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CustomTextField(
+                hintText: 'Password',
+                controller: _passwordController,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Navigator.pushNamed(context, 'Medicine');
+                    } else {
+                      print("Not Validated");
+                    }
+                  },
+                  child: Text('Login')),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'dont\'t have an account?',
+                    style: TextStyle(
+                      color: Color(0xff17A4A1),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, 'RegisterPage');
+                    },
+                    child: Text(
+                      ' Register',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(
+                flex: 3,
+              ),
+            ],
+          ),
         ),
       ),
     );
